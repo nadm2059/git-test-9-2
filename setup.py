cb66fecdfde62a8699fe7a04c2d9458d55d3dff6
nadm2059 <n.s.michaelho2004@gmail.com>
2025-09-03 00:15:02 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="hello-pkg-demo",        # package name
+    version="0.1.0",              # version
+    py_modules=["app"],          # your python file (hello.py)
+    install_requires=[],           # dependencies if any
+    author="Your Name",
+    description="A simple Python program that prints Hello -- version 1!",
+)
